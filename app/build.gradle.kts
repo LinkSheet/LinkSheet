15b343b312e874e20471aa45f687c46ef7893d43
1fexd <PGFvy85nC@protonmail.com>
2023-02-21 19:11:04 +0100

    chore: Set version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

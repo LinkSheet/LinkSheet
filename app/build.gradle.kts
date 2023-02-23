6074761912ba85be25f78f0c3059f42c6b202097
1fexd <PGFvy85nC@protonmail.com>
2023-02-23 18:27:07 +0100

    chore: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "fe.linksheet"
         minSdk = 26
         targetSdk = 33
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 4
+        versionName = "0.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

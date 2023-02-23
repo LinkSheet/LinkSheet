a440e6703571f023f326ce65cf143dad53e5d306
1fexd <PGFvy85nC@protonmail.com>
2023-02-23 18:39:20 +0100

    chore: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "fe.linksheet"
         minSdk = 26
         targetSdk = 33
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
         setProperty("archivesBaseName", "LinkSheet-$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- app/src/main/java/fe/linksheet/composable/settings/SettingsViewModel.kt
+++ app/src/main/java/fe/linksheet/composable/settings/SettingsViewModel.kt
@@ -1,16 +1,13 @@
 package fe.linksheet.composable.settings
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.content.pm.verify.domain.DomainVerificationManager
 import android.content.pm.verify.domain.DomainVerificationUserState
 import android.net.Uri
 import android.os.Build
 import android.provider.Settings
-import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
@@ -24,8 +21,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
-import org.lsposed.hiddenapibypass.HiddenApiBypass
-import java.lang.reflect.Field
 
 
 class SettingsViewModel : ViewModel(), KoinComponent {
@@ -101,27 +96,27 @@ class SettingsViewModel : ViewModel(), KoinComponent {
         }
     }
 
-    companion object {
-        const val PRIVATE_FLAG_HAS_DOMAIN_URLS = (1 shl 4)
-    }
-
-    @SuppressLint("DiscouragedPrivateApi")
-    private fun doesAppHandleLinks(context: Context, packageName: String): Boolean? {
-        val packageInfo = context.packageManager.getPackageInfo(packageName, 0)
-
-        val field = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val fields =
-                HiddenApiBypass.getInstanceFields(ApplicationInfo::class.java) as List<Field>
-            fields.find { it.name == "privateFlags" }?.get(packageInfo.applicationInfo)
-        } else {
-            ApplicationInfo::class.java.getDeclaredField("privateFlags").apply {
-                this.isAccessible = true
-            }.get(packageInfo.applicationInfo)
-        }
-
-        return field?.let {
-            // https://android.googlesource.com/platform/frameworks/base/+/android-8.0.0_r4/cmds/pm/src/com/android/commands/pm/Pm.java#898
-            it.toString().toInt() and PRIVATE_FLAG_HAS_DOMAIN_URLS != 0
-        }
-    }
+//    companion object {
+//        const val PRIVATE_FLAG_HAS_DOMAIN_URLS = (1 shl 4)
+//    }
+//
+//    @SuppressLint("DiscouragedPrivateApi")
+//    private fun doesAppHandleLinks(context: Context, packageName: String): Boolean? {
+//        val packageInfo = context.packageManager.getPackageInfo(packageName, 0)
+//
+//        val field = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+//            val fields =
+//                HiddenApiBypass.getInstanceFields(ApplicationInfo::class.java) as List<Field>
+//            fields.find { it.name == "privateFlags" }?.get(packageInfo.applicationInfo)
+//        } else {
+//            ApplicationInfo::class.java.getDeclaredField("privateFlags").apply {
+//                this.isAccessible = true
+//            }.get(packageInfo.applicationInfo)
+//        }
+//
+//        return field?.let {
+//            // https://android.googlesource.com/platform/frameworks/base/+/android-8.0.0_r4/cmds/pm/src/com/android/commands/pm/Pm.java#898
+//            it.toString().toInt() and PRIVATE_FLAG_HAS_DOMAIN_URLS != 0
+//        }
+//    }
 }

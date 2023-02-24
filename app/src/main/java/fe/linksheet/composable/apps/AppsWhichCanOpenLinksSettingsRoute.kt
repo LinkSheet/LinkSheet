759800c38358a9309b943eb750aa4609c2b3ea97
1fexd <PGFvy85nC@protonmail.com>
2023-02-24 19:14:39 +0100

    feat: Add text when filter did not result in any apps, loader when apps are loading

--- app/src/main/java/fe/linksheet/composable/apps/AppsWhichCanOpenLinksSettingsRoute.kt
+++ app/src/main/java/fe/linksheet/composable/apps/AppsWhichCanOpenLinksSettingsRoute.kt
@@ -50,27 +50,36 @@ fun AppsWhichCanOpenLinksSettingsRoute(
     var refreshing by remember { mutableStateOf(false) }
     var filter by remember { mutableStateOf("") }
 
-    val fetch: suspend CoroutineScope.() -> Unit = {
-        refreshing = true
+    val fetch: suspend CoroutineScope.(Boolean) -> Unit = { fetchRefresh ->
+        if (fetchRefresh) {
+            refreshing = true
+        }
+
         viewModel.loadAppsWhichCanHandleLinksAsync(context, manager).await()
         viewModel.filterWhichAppsCanHandleLinksAsync(filter).await()
 
-        delay(100)
-        refreshing = false
+        if (fetchRefresh) {
+            delay(100)
+            refreshing = false
+        }
     }
 
     val lifecycleState = LocalLifecycleOwner.current.lifecycle.observeAsState()
 
     LaunchedEffect(lifecycleState.first) {
-        if (lifecycleState.first == Lifecycle.Event.ON_RESUME) {
-            fetch()
+        if (lifecycleState.first == Lifecycle.Event.ON_RESUME && lifecycleState.second != Lifecycle.Event.ON_START) {
+            fetch(false)
         }
+    }
+
+    LaunchedEffect(Unit) {
+        fetch(false)
     }
 
     val refreshScope = rememberCoroutineScope()
 
     val state = rememberPullRefreshState(refreshing, onRefresh = {
-        refreshScope.launch(block = fetch)
+        refreshScope.launch(block = { fetch(true) })
     })
 
     Box(modifier = Modifier.pullRefresh(state)) {
@@ -125,42 +134,58 @@ fun AppsWhichCanOpenLinksSettingsRoute(
 
             Spacer(modifier = Modifier.height(10.dp))
 
-            LazyColumn(modifier = Modifier.fillMaxSize(), content = {
-                if (!refreshing) {
-                    items(
-                        viewModel.whichAppsCanHandleLinksFiltered,
-                        key = { it.packageName }
-                    ) { info ->
-                        ClickableRow(
-                            padding = 5.dp,
-                            onClick = {
-                                viewModel.openOpenByDefaultSettings(
-                                    context,
-                                    info.packageName
+            if (viewModel.whichAppsCanHandleLinksFiltered.isNotEmpty()) {
+                LazyColumn(modifier = Modifier.fillMaxSize(), content = {
+                    if (!refreshing) {
+                        items(
+                            viewModel.whichAppsCanHandleLinksFiltered,
+                            key = { it.packageName }
+                        ) { info ->
+                            ClickableRow(
+                                padding = 5.dp,
+                                onClick = {
+                                    viewModel.openOpenByDefaultSettings(
+                                        context,
+                                        info.packageName
+                                    )
+                                },
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Image(
+                                    bitmap = info.getBitmap(context),
+                                    contentDescription = info.displayLabel,
+                                    modifier = Modifier.size(42.dp)
                                 )
-                            },
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Image(
-                                bitmap = info.getBitmap(context),
-                                contentDescription = info.displayLabel,
-                                modifier = Modifier.size(42.dp)
-                            )
 
-                            Spacer(modifier = Modifier.width(10.dp))
+                                Spacer(modifier = Modifier.width(10.dp))
 
-                            Column {
-                                Text(
-                                    text = info.displayLabel, fontSize = 16.sp,
-                                    color = MaterialTheme.colorScheme.onSurface
-                                )
+                                Column {
+                                    Text(
+                                        text = info.displayLabel, fontSize = 16.sp,
+                                        color = MaterialTheme.colorScheme.onSurface
+                                    )
+                                }
                             }
-                        }
 
-                        Spacer(modifier = Modifier.height(10.dp))
+                            Spacer(modifier = Modifier.height(10.dp))
+                        }
+                    }
+                })
+            } else {
+                Column(
+                    modifier = Modifier.fillMaxWidth().fillMaxHeight(0.3f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    if (viewModel.whichAppsCanHandleLinks.isEmpty()) {
+                        CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+                    } else {
+                        Text(
+                            text = stringResource(id = R.string.no_such_app_found),
+                        )
                     }
                 }
-            })
+            }
         }
     }
 }

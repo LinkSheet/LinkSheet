2517b9ce72614e1f2cd5a7d9440588fd612ae533
1fexd <PGFvy85nC@protonmail.com>
2023-02-24 19:46:29 +0100

    feat: Cleanup intent

--- app/src/main/java/fe/linksheet/util/IntentUtil.kt
+++ app/src/main/java/fe/linksheet/util/IntentUtil.kt
@@ -1,9 +1,14 @@
 package fe.linksheet.util
 
-import android.app.Activity
 import android.content.Intent
+import fe.linksheet.extension.getUri
 
 fun Intent.sourceIntent() = Intent(this).apply {
     component = null
+    action = Intent.ACTION_VIEW
+    data = this.getUri()
     flags = flags and Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS.inv()
 }
+
+//{ act=android.intent.action.SEND typ=text/plain flg=0x10800001 cmp=fe.linksheet/.activity.bottomsheet.BottomSheetActivity clip={text/plain {T(59)}} (has extras) }
+//{ act=android.intent.action.VIEW dat=https://twitter.com/... flg=0x10800000 cmp=fe.linksheet/.activity.bottomsheet.BottomSheetActivity (has extras) }

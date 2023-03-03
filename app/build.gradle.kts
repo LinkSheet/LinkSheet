13b369ef1598bca465b5459a24f39fd70bc7dff3
1fexd <PGFvy85nC@protonmail.com>
2023-03-03 23:59:07 +0100

    chore: Bump fastforwardkt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
 
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.github.1fexd:clearurlkt:0.0.9")
-    implementation("com.github.1fexd:fastforwardkt:0.0.8")
+    implementation("com.github.1fexd:fastforwardkt:0.0.9")
     implementation("com.gitlab.grrfe.httpkt:core:13.0.0-alpha.31")
     implementation("com.gitlab.grrfe.httpkt:ext-gson:13.0.0-alpha.31")
     implementation("com.gitlab.grrfe:GSONKtExtensions:2.1.2")

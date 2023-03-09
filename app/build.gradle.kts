7d874edeb60552e1575b0260d349843b47e0fbf9
1fexd <PGFvy85nC@protonmail.com>
2023-03-09 19:03:09 +0100

    chore: Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "fe.linksheet"
         minSdk = 25
         targetSdk = 33
-        versionCode = 25
-        versionName = "0.0.25-debug"
+        versionCode = 26
+        versionName = "0.0.26-debug"
         setProperty("archivesBaseName", "LinkSheet-$versionName")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"

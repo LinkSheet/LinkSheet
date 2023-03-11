b9ee835ed9a8fa3722fe8f03d48c522a398c2ce6
1fexd <PGFvy85nC@protonmail.com>
2023-03-11 11:56:17 +0100

    #35 chore: Bump clearurlkt to fix crash

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ dependencies {
     implementation("com.google.accompanist:accompanist-navigation-animation:0.29.1-alpha")
 
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.github.1fexd:clearurlkt:0.0.10")
+    implementation("com.github.1fexd:clearurlkt:0.0.11")
     implementation("com.github.1fexd:fastforwardkt:0.0.11")
     implementation("com.github.1fexd:libredirectkt:0.0.9")
 

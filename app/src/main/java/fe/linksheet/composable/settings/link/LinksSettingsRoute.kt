583ffff10d9ce4e762ce247f846f8f03519d85f8
1fexd <PGFvy85nC@protonmail.com>
2023-06-01 20:07:17 +0200

    feat: Animate operations card in LinksSettingsRoute

--- app/src/main/java/fe/linksheet/composable/settings/link/LinksSettingsRoute.kt
+++ app/src/main/java/fe/linksheet/composable/settings/link/LinksSettingsRoute.kt
@@ -1,15 +1,28 @@
 package fe.linksheet.composable.settings.link
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -57,7 +70,7 @@ fun LinksSettingsRoute(
             contentPadding = PaddingValues(horizontal = 5.dp)
         ) {
             stickyHeader(key = "header") {
-                if (operations.any { it.value.value }) {
+                AnimatedVisibility(visible = operations.any { it.value.value }) {
                     Column(
                         modifier = Modifier
                             .fillMaxWidth()
@@ -84,16 +97,17 @@ fun LinksSettingsRoute(
                                 Column(modifier = Modifier.padding(10.dp)) {
                                     HeadlineText(headlineId = R.string.order_of_operation)
 
-                                    operations.filter { (_, enabled) -> enabled.value }
-                                        .forEach { (stringId, _) ->
+                                    operations.forEach { (stringId, enabled) ->
+                                        AnimatedVisibility(visible = enabled.value) {
                                             SubtitleText(subtitleId = stringId)
                                         }
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    Spacer(modifier = Modifier.height(5.dp))
+                        Spacer(modifier = Modifier.height(10.dp))
+                    }
                 }
             }
 

9e0e5e495203fc105866876f43027dcd6b695f7e
1fexd <PGFvy85nC@protonmail.com>
2023-08-05 17:20:53 +0200

    fix: Use correct user id

--- app/src/main/java/dev/zwander/shared/shizuku/ShizukuService.kt
+++ app/src/main/java/dev/zwander/shared/shizuku/ShizukuService.kt
@@ -1,15 +1,11 @@
 package dev.zwander.shared.shizuku
 
 import android.content.Context
-import android.os.Build
 import android.os.UserHandle
-import android.util.Log
 import androidx.annotation.Keep
 import dev.zwander.shared.IShizukuService
-import dev.zwander.shared.data.VerifyResult
 import fe.linksheet.util.AndroidVersion
 import fe.processlauncher.launchProcess
-import rikka.shizuku.Shizuku
 import kotlin.system.exitProcess
 
 class ShizukuService : IShizukuService.Stub {
@@ -23,7 +19,7 @@ class ShizukuService : IShizukuService.Stub {
         try {
             return if (AndroidVersion.AT_LEAST_API_31_S) {
                 launchProcess("pm", "set-app-links-allowed",
-                    "--user", Shizuku.getUid().toString(),
+                    "--user", UserHandle.myUserId().toString(),
                     "--package", packageName,
                     enabled.toString()
                 ).waitFor()

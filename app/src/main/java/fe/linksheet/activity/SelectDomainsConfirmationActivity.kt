2dc8ef9aef2bec665cb3567f31502c4f09881bfd
1fexd <PGFvy85nC@protonmail.com>
2023-08-17 10:18:56 +0200

    chore: Use guards instead of nesting, make text fill the whole width

--- app/src/main/java/fe/linksheet/activity/SelectDomainsConfirmationActivity.kt
+++ app/src/main/java/fe/linksheet/activity/SelectDomainsConfirmationActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -25,6 +26,7 @@ import fe.linksheet.composable.util.DialogContent
 import fe.linksheet.composable.util.DialogSpacer
 import fe.linksheet.composable.util.HeadlineText
 import fe.linksheet.composable.util.SubtitleText
+import fe.linksheet.extension.android.initPadding
 import fe.linksheet.interconnect.StringParceledListSlice
 import fe.linksheet.module.database.entity.PreferredApp
 import fe.linksheet.module.repository.PreferredAppRepository
@@ -40,7 +42,12 @@ class SelectDomainsConfirmationActivity : ComponentActivity() {
         const val EXTRA_CALLING_PACKAGE = "calling_package"
         const val EXTRA_DOMAINS = "domains"
 
-        fun start(context: Context, callingPackage: String, callingComponent: String, domains: StringParceledListSlice) {
+        fun start(
+            context: Context,
+            callingPackage: String,
+            callingComponent: String,
+            domains: StringParceledListSlice
+        ) {
             val intent = Intent(context, SelectDomainsConfirmationActivity::class.java)
 
             intent.action = ACTION_CONFIRM
@@ -59,88 +66,104 @@ class SelectDomainsConfirmationActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (intent?.action == ACTION_CONFIRM) {
-            val callingComponent = intent.getStringExtra(EXTRA_CALLING_COMPONENT)
-            val callingPackage = intent.getStringExtra(EXTRA_CALLING_PACKAGE)
-            val domains = IntentCompat.getParcelableExtra(intent, EXTRA_DOMAINS, StringParceledListSlice::class.java)?.list
+        if (intent?.action != ACTION_CONFIRM) return
 
-            if (callingPackage != null && domains != null && callingComponent != null) {
-                val appLabel = try {
-                    @Suppress("DEPRECATION")
-                    packageManager.getApplicationInfo(callingPackage, 0)
-                        .loadLabel(packageManager)
-                } catch (e: PackageManager.NameNotFoundException) {
-                    callingPackage
-                }
+        val callingComponent = intent.getStringExtra(EXTRA_CALLING_COMPONENT)
+        val callingPackage = intent.getStringExtra(EXTRA_CALLING_PACKAGE)
+        val domains = IntentCompat.getParcelableExtra(
+            intent,
+            EXTRA_DOMAINS,
+            StringParceledListSlice::class.java
+        )?.list
 
-                setContent {
-                    val scope = rememberCoroutineScope()
+        if (callingPackage == null || domains == null || callingComponent == null) {
+            finish()
+            return
+        }
 
-                    AppTheme {
-                        Box(modifier = Modifier.fillMaxWidth()) {
-                            val dialog = dialogHelper(
-                                state = domains,
-                                onClose = { confirmed ->
-                                    scope.launch(Dispatchers.IO) {
-                                        if (confirmed == true) {
-                                            repository.insert(domains.map {
-                                                PreferredApp(
-                                                    host = it,
-                                                    packageName = callingPackage,
-                                                    component = callingComponent,
-                                                    alwaysPreferred = true,
-                                                )
-                                            })
-                                        }
+        initPadding()
 
-                                        finish()
+        val appLabel = try {
+            @Suppress("DEPRECATION")
+            packageManager.getApplicationInfo(callingPackage, 0)
+                .loadLabel(packageManager)
+        } catch (e: PackageManager.NameNotFoundException) {
+            callingPackage
+        }
+
+        setContent {
+            val scope = rememberCoroutineScope()
+
+            AppTheme {
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                ) {
+                    val dialog = dialogHelper(
+                        state = domains,
+                        onClose = { confirmed ->
+                            scope.launch(Dispatchers.IO) {
+                                if (confirmed == true) {
+                                    repository.insert(domains.map {
+                                        PreferredApp(
+                                            host = it,
+                                            packageName = callingPackage,
+                                            component = callingComponent,
+                                            alwaysPreferred = true,
+                                        )
+                                    })
+                                }
+
+                                finish()
+                            }
+                        },
+                        dynamicHeight = true,
+                        notifyCloseNoState = true,
+                    ) { state, close ->
+                        DialogColumn {
+                            HeadlineText(headlineId = R.string.domain_selection_confirmation_title)
+                            SubtitleText(
+                                subtitle = stringResource(
+                                    id = R.string.domain_selection_confirmation_subtitle,
+                                    appLabel
+                                )
+                            )
+                            DialogSpacer()
+                            DialogContent(
+                                items = state,
+                                key = { it },
+                                bottomRow = {
+                                    TextButton(
+                                        onClick = {
+                                            close(false)
+                                        },
+                                    ) {
+                                        Text(text = stringResource(id = R.string.no))
+                                    }
+
+                                    TextButton(
+                                        onClick = {
+                                            close(true)
+                                        },
+                                        colors = ButtonDefaults.textButtonColors(
+                                            contentColor = MaterialTheme.colorScheme.error,
+                                        ),
+                                    ) {
+                                        Text(text = stringResource(id = R.string.yes))
                                     }
                                 },
-                                dynamicHeight = true,
-                                notifyCloseNoState = true,
-                            ) { state, close ->
-                                DialogColumn {
-                                    HeadlineText(headlineId = R.string.domain_selection_confirmation_title)
-                                    SubtitleText(subtitle = stringResource(id = R.string.domain_selection_confirmation_subtitle, appLabel))
-                                    DialogSpacer()
-                                    DialogContent(
-                                        items = state,
-                                        key = { it },
-                                        bottomRow = {
-                                            TextButton(
-                                                onClick = {
-                                                    close(false)
-                                                },
-                                            ) {
-                                                Text(text = stringResource(id = R.string.no))
-                                            }
-
-                                            TextButton(
-                                                onClick = {
-                                                    close(true)
-                                                },
-                                                colors = ButtonDefaults.textButtonColors(
-                                                    contentColor = MaterialTheme.colorScheme.error,
-                                                ),
-                                            ) {
-                                                Text(text = stringResource(id = R.string.yes))
-                                            }
-                                        },
-                                        content = {
-                                            Text(text = it)
-                                        },
-                                    )
-                                }
-                            }
-
-                            LaunchedEffect(null) {
-                                dialog.open()
-                            }
+                                content = {
+                                    Row(modifier = Modifier.fillMaxWidth()) {
+                                        Text(text = it)
+                                    }
+                                },
+                            )
                         }
                     }
+
+                    LaunchedEffect(null) {
+                        dialog.open()
+                    }
                 }
-            } else {
-                finish()
             }
         }
     }

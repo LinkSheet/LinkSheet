0fc50dbe22778927a5610c65b3b264ff81766f8c
1fexd <PGFvy85nC@protonmail.com>
2023-10-25 13:21:49 +0200

    #226 feat: Add (broken/for now at least) test for UriHasher

---
+++ app/src/test/java/fe/linksheet/UriHasherTest.kt
@@ -0,0 +1,24 @@
+package fe.linksheet
+
+import fe.linksheet.util.CryptoUtil
+import org.junit.Test
+import javax.crypto.Mac
+
+class UriHasherTest {
+    private val mac: Mac
+
+    init {
+        val hmacSha256 = CryptoUtil.HmacSha("HmacSHA256", 64)
+        val test = "test"
+        val key = test.repeat(hmacSha256.keySize / test.length).toByteArray()
+
+        mac = CryptoUtil.makeHmac(hmacSha256.algorithm, key)
+    }
+
+    @Test
+    fun test() {
+//        assertEquals(buildHashedUriString("https://google.com/henlo/fren", mac), "https://b60687.8fc4dccb1dcf/b3901e/452116/")
+        // TODO: query parameters are currently broken. Let's fix this later, there's more important stuff to do (tracked in #226)
+//        assertEquals(buildHashedUriString("https://google.com?we=is&fren=yes", mac), "https://b60687.8fc4dccb1dcf/b3901e/452116/")
+    }
+}

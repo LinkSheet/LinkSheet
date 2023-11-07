37e904222001d870219dca55b20e0c0bc65233de
1fexd <PGFvy85nC@protonmail.com>
2023-11-07 13:51:31 +0100

    chore: Rework remote functions

--- app/src/foss/java/fe/linksheet/LinkSheetAppConfig.kt
+++ app/src/foss/java/fe/linksheet/LinkSheetAppConfig.kt
@@ -3,7 +3,14 @@ package fe.linksheet
 import fe.linksheet.config.AppConfig
 
 object LinkSheetAppConfig : AppConfig {
-    override fun showDonationBanner(): Boolean {
-        return true
+    override fun isPro(): Boolean {
+        return false
     }
+
+    override fun showDonationBanner(): Boolean {
+        return !isPro()
+    }
+
+    override fun supabaseHost(): String = ""
+    override fun supabaseApiKey(): String = ""
 }
--- app/src/main/java/fe/linksheet/module/preference/AppPreferenceRepository.kt
+++ app/src/main/java/fe/linksheet/module/preference/AppPreferenceRepository.kt
@@ -3,6 +3,8 @@ package fe.linksheet.module.preference
 import android.content.Context
 import com.google.gson.Gson
 import fe.android.preference.helper.PreferenceRepository
+import fe.android.preference.helper.compose.getBooleanState
+import fe.linksheet.LinkSheetAppConfig
 import org.json.JSONObject
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -12,4 +14,14 @@ val preferenceRepositoryModule = module {
 }
 
 class AppPreferenceRepository(context: Context, val gson: Gson) : PreferenceRepository(context) {
+    private val followRedirectsExternalService = getBooleanState(AppPreferences.followRedirectsExternalService)
+    private val amp2HtmlExternalService = getBooleanState(AppPreferences.amp2HtmlExternalService)
+
+    init {
+        // Ensure backwards compatibility as this feature was previously included in non-pro versions
+        if (!LinkSheetAppConfig.isPro()) {
+            followRedirectsExternalService.updateState(false)
+            amp2HtmlExternalService.updateState(false)
+        }
+    }
 }

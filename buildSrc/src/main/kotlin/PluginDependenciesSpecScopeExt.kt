020a75570c1d3c0641143efc7e15aee42cf70f31
1fexd <PGFvy85nC@protonmail.com>
2024-01-04 12:52:24 +0100

    chore: Bump dependencies

--- buildSrc/src/main/kotlin/PluginDependenciesSpecScopeExt.kt
+++
@@ -1,8 +0,0 @@
-import org.gradle.api.provider.Provider
-import org.gradle.kotlin.dsl.PluginDependenciesSpecScope
-import org.gradle.plugin.use.PluginDependency
-import org.gradle.plugin.use.PluginDependencySpec
-
-fun PluginDependenciesSpecScope.id(plugin: Provider<PluginDependency>): PluginDependencySpec {
-    return id(plugin.get().pluginId)
-}

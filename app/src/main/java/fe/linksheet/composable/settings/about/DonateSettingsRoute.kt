df6d6343b41f19c83f3b3a90b27bc66acad0f95b
1fexd <PGFvy85nC@protonmail.com>
2024-01-21 11:34:17 +0100

    chore: Cleanup

--- app/src/main/java/fe/linksheet/composable/settings/about/DonateSettingsRoute.kt
+++ app/src/main/java/fe/linksheet/composable/settings/about/DonateSettingsRoute.kt
@@ -125,7 +125,6 @@ fun DonateSettingsRoute(
                                 enabled = true
                             )
                         }
-
                     }
                 }
             }

a2af766105053e849db7864213da7d0e9948d314
1fexd <PGFvy85nC@protonmail.com>
2024-02-22 20:51:19 +0100

    #329 chore: Fix typo in i2p

--- app/src/main/java/fe/linksheet/util/Darknet.kt
+++ app/src/main/java/fe/linksheet/util/Darknet.kt
@@ -3,7 +3,7 @@ package fe.linksheet.util
 import android.net.Uri
 
 enum class Darknet(val displayName: String, val tld: String) {
-    I2P("I2P2", "i2p"), Tor("Tor", "onion");
+    I2P("I2P", "i2p"), Tor("Tor", "onion");
 
     companion object {
         fun getOrNull(uri: Uri): Darknet? {

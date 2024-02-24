cd5d5615f56bf52edf3bc4e3f22f15234165456d
1fexd <PGFvy85nC@protonmail.com>
2024-02-24 22:49:49 +0100

    fix: Crash if flow is empty

--- app/src/main/java/fe/linksheet/module/repository/AppSelectionHistoryRepository.kt
+++ app/src/main/java/fe/linksheet/module/repository/AppSelectionHistoryRepository.kt
@@ -3,7 +3,7 @@ package fe.linksheet.module.repository
 import android.net.Uri
 import fe.linksheet.module.database.dao.AppSelectionHistoryDao
 import fe.linksheet.module.database.entity.AppSelectionHistory
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.map
 
 
@@ -14,6 +14,6 @@ class AppSelectionHistoryRepository(private val dao: AppSelectionHistoryDao) {
     suspend fun getLastUsedForHostGroupedByPackage(uri: Uri?) = uri?.host?.let { host ->
         dao.getLastUsedForHostGroupedByPackage(host).map { appSelections ->
             appSelections.associate { it.packageName to it.maxLastUsed }
-        }.first()
+        }.firstOrNull()
     }
 }

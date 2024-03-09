1dfc3df876deb0b371c19c3322c3cd90618a50dd
1fexd <PGFvy85nC@protonmail.com>
2024-03-09 19:55:44 +0100

    chore: Cleanup

--- app/src/main/java/fe/linksheet/customtab/LinkSheetCustomTabsService.kt
+++ app/src/main/java/fe/linksheet/customtab/LinkSheetCustomTabsService.kt
@@ -6,39 +6,47 @@ import androidx.browser.customtabs.CustomTabsService
 import androidx.browser.customtabs.CustomTabsSessionToken
 
 class LinkSheetCustomTabsService : CustomTabsService() {
-    override fun warmup(flags: Long) = true
-    override fun newSession(sessionToken: CustomTabsSessionToken) = true
+    override fun warmup(flags: Long): Boolean {
+        return true
+    }
+
+    override fun newSession(sessionToken: CustomTabsSessionToken): Boolean {
+        return true
+    }
+
     override fun mayLaunchUrl(
         sessionToken: CustomTabsSessionToken,
         url: Uri?,
         extras: Bundle?,
-        otherLikelyBundles: MutableList<Bundle>?
-    ) = true
+        otherLikelyBundles: MutableList<Bundle>?,
+    ): Boolean {
+        return true
+    }
 
-    override fun extraCommand(commandName: String, args: Bundle?): Bundle? = null
-    override fun updateVisuals(sessionToken: CustomTabsSessionToken, bundle: Bundle?) = true
-    override fun requestPostMessageChannel(
-        sessionToken: CustomTabsSessionToken,
-        postMessageOrigin: Uri
-    ) = true
+    override fun extraCommand(commandName: String, args: Bundle?): Bundle? {
+        return null
+    }
 
-    override fun postMessage(
-        sessionToken: CustomTabsSessionToken,
-        message: String,
-        extras: Bundle?
-    ) = RESULT_SUCCESS
+    override fun requestPostMessageChannel(sessionToken: CustomTabsSessionToken, postMessageOrigin: Uri): Boolean {
+        return false
+    }
+
+    override fun postMessage(sessionToken: CustomTabsSessionToken, message: String, extras: Bundle?): Int {
+        return RESULT_FAILURE_DISALLOWED
+    }
 
     override fun validateRelationship(
         sessionToken: CustomTabsSessionToken,
         relation: Int,
         origin: Uri,
-        extras: Bundle?
+        extras: Bundle?,
     ) = true
 
-    override fun receiveFile(
-        sessionToken: CustomTabsSessionToken,
-        uri: Uri,
-        purpose: Int,
-        extras: Bundle?
-    ) = true
+    override fun updateVisuals(sessionToken: CustomTabsSessionToken, bundle: Bundle?): Boolean {
+        return false
+    }
+
+    override fun receiveFile(sessionToken: CustomTabsSessionToken, uri: Uri, purpose: Int, extras: Bundle?): Boolean {
+        return false
+    }
 }

fcec643b767325c63448b07343dbd285f959fa86
1fexd <PGFvy85nC@protonmail.com>
2024-03-30 23:44:32 +0100

    chore: Cleanup

--- app/src/main/java/fe/linksheet/activity/bottomsheet/failure/FailureSheetColumn.kt
+++ app/src/main/java/fe/linksheet/activity/bottomsheet/failure/FailureSheetColumn.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +14,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fe.linksheet.R
-import fe.linksheet.activity.LoadingIndicator
 import fe.linksheet.activity.bottomsheet.BottomSheetActivityImpl.Companion.buttonPadding
 import fe.linksheet.activity.bottomsheet.BottomSheetActivityImpl.Companion.buttonRowHeight
 import fe.linksheet.resolver.BottomSheetResult

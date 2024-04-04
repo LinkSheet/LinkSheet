8007290a0906e7c6deabf987404f0ce69395268e
1fexd <PGFvy85nC@protonmail.com>
2024-04-04 16:36:35 +0200

    fix: New query manager crashes

--- app/src/main/experiment/fe/linksheet/experiment/new/query/manager/query/PackageQueryManager.kt
+++ app/src/main/experiment/fe/linksheet/experiment/new/query/manager/query/PackageQueryManager.kt
@@ -21,14 +21,27 @@ object PackageQueryManager {
         val mainIntent = Intent(Intent.ACTION_MAIN, null)
         mainIntent.addCategory(Intent.CATEGORY_LAUNCHER)
 
+        val viewIntent = Intent(Intent.ACTION_VIEW, uri)
+
+        val packageManager = context.packageManager
         val result = context.packageManager.queryIntentActivitiesCompat(mainIntent, PackageManager.MATCH_ALL)
-        return result.filter { it.canHandle(dvm, host) }
+        return result.asSequence()
+            .filter { it.canHandle(dvm, host) }
+            .filter { it.hasActivity(packageManager, viewIntent) != null }
+            .toList()
     }
 
     @RequiresApi(Build.VERSION_CODES.S)
     private fun ResolveInfo.canHandle(dvm: DomainVerificationManager, host: String): Boolean {
         // TODO: Does this work for wildcard subdomains? (*.example.org?)
         return dvm.getDomainVerificationUserState(activityInfo.packageName)?.hostToStateMap?.containsKey(host) == true
+    }
+
+    private fun ResolveInfo.hasActivity(packageManager: PackageManager, viewIntent: Intent): ResolveInfo? {
+        return packageManager.resolveActivity(
+            Intent(viewIntent).setPackage(activityInfo.packageName),
+            PackageManager.MATCH_DEFAULT_ONLY
+        )
     }
 
     private fun PackageManager.queryIntentActivitiesCompat(intent: Intent, flags: Int): MutableList<ResolveInfo> {

a6233f95d7b739077455d227054b8e1b60a0ea83
1fexd <PGFvy85nC@protonmail.com>
2024-04-26 14:43:56 +0200

    fix: Signature verification issues (3)

--- app/src/main/java/fe/linksheet/Constants.kt
+++ app/src/main/java/fe/linksheet/Constants.kt
@@ -13,9 +13,5 @@ const val linksheetGithub = "$linksheetOrg/LinkSheet"
 const val linkSheetCompatGithubReleases = "$linksheetOrg/compat/releases"
 
 const val shizukuDownload = "https://shizuku.rikka.app/download"
-val officialSigningKeys = mapOf(
-    "C2A8B18C328DFB39C896491757ED11C145D3ACCA43212FA3DE362433C416AAA9" to AppSignature.SignatureBuildType.Manual,
-    "3FCF7675BC90E239892C7262499DCC9F8CE6A52B7E58D02B56AA60CA669D6701" to AppSignature.SignatureBuildType.GithubPipeline,
-)
 
 val lineSeparator: String = System.lineSeparator()

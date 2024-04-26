d811426d56e7d0b287a30ac10f752fd033d9ec84
1fexd <PGFvy85nC@protonmail.com>
2024-04-26 14:34:01 +0200

    fix: Signature verification issues (2)

--- app/src/main/java/fe/linksheet/Constants.kt
+++ app/src/main/java/fe/linksheet/Constants.kt
@@ -1,5 +1,7 @@
 package fe.linksheet
 
+import fe.linksheet.util.AppSignature
+
 private const val github = "https://github.com"
 private const val linksheetOrg = "$github/LinkSheet"
 
@@ -11,5 +13,9 @@ const val linksheetGithub = "$linksheetOrg/LinkSheet"
 const val linkSheetCompatGithubReleases = "$linksheetOrg/compat/releases"
 
 const val shizukuDownload = "https://shizuku.rikka.app/download"
+val officialSigningKeys = mapOf(
+    "C2A8B18C328DFB39C896491757ED11C145D3ACCA43212FA3DE362433C416AAA9" to AppSignature.SignatureBuildType.Manual,
+    "3FCF7675BC90E239892C7262499DCC9F8CE6A52B7E58D02B56AA60CA669D6701" to AppSignature.SignatureBuildType.GithubPipeline,
+)
 
 val lineSeparator: String = System.lineSeparator()
--- app/src/main/java/fe/linksheet/util/AppSignature.kt
+++ app/src/main/java/fe/linksheet/util/AppSignature.kt
@@ -1,31 +1,39 @@
 package fe.linksheet.util
 
 import android.content.Context
-import android.content.pm.getSignature
+import android.content.pm.PackageManager
 import androidx.annotation.StringRes
 import fe.linksheet.BuildConfig
 import fe.linksheet.R
-import fe.linksheet.lib.flavors.LinkSheetApp
-import fe.linksheet.lib.flavors.Signature
+import fe.linksheet.officialSigningKeys
+import java.util.Locale
 
 object AppSignature {
     private lateinit var buildType: SignatureBuildType
 
-    enum class SignatureBuildType(val signature: Signature?, @StringRes val stringRes: Int) {
-        Manual(LinkSheetApp.Signature, R.string.manual_build),
-        CI(LinkSheetApp.SignatureCI, R.string.github_pipeline_build),
-        Unofficial(null, R.string.built_by_error),
-        Debug(null, R.string.debug_build)
+    enum class SignatureBuildType(@StringRes val stringRes: Int) {
+        Manual(R.string.manual_build),
+        GithubPipeline(R.string.github_pipeline_build),
+        Unofficial(R.string.built_by_error),
+        Debug(R.string.debug_build)
     }
 
     fun checkSignature(context: Context): SignatureBuildType {
         if (BuildConfig.DEBUG) return SignatureBuildType.Debug
         if (this::buildType.isInitialized) return buildType
 
-        val signature = context.packageManager.getSignature(context.packageName)
-        val knownSignature = LinkSheetApp.LinkSheet.isValidSignature(signature.toByteArray())
-        buildType = SignatureBuildType.entries.first { it.signature == knownSignature }
+        val signature = context.packageManager.getPackageInfo(
+            context.packageName,
+            PackageManager.GET_SIGNATURES
+        ).signatures[0]
 
-        return buildType
+        val certFingerprint = CryptoUtil.sha256Hex(
+            signature.toByteArray()
+        ).uppercase(
+            Locale.getDefault()
+        )
+        return officialSigningKeys.getOrDefault(certFingerprint, SignatureBuildType.Unofficial).also {
+            buildType = it
+        }
     }
 }

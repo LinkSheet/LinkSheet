b1aa846ab33fae5205d9a3a5293f82944b3414fe
1fexd <PGFvy85nC@protonmail.com>
2024-04-27 07:51:12 +0200

    fix: Pad FAB properly

--- app/src/main/experiment/fe/linksheet/experiment/ui/overhaul/composable/page/settings/advanced/ExperimentsSettingsRoute.kt
+++ app/src/main/experiment/fe/linksheet/experiment/ui/overhaul/composable/page/settings/advanced/ExperimentsSettingsRoute.kt
@@ -1,15 +1,19 @@
 package fe.linksheet.experiment.ui.overhaul.composable.page.settings.advanced
 
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Restore
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import fe.linksheet.R
 import fe.linksheet.experiment.ui.overhaul.composable.ContentTypeDefaults
-import fe.linksheet.experiment.ui.overhaul.composable.component.list.item.ShapeListItemDefaults
 import fe.linksheet.experiment.ui.overhaul.composable.component.card.AlertCard
 import fe.linksheet.experiment.ui.overhaul.composable.component.list.item.type.SwitchListItem
 import fe.linksheet.experiment.ui.overhaul.composable.component.page.SaneScaffoldSettingsPage
@@ -25,7 +29,10 @@ fun NewExperimentsSettingsRoute(
         headline = stringResource(id = R.string.experiments),
         onBackPressed = onBackPressed,
         floatingActionButton = {
-            FloatingActionButton(onClick = { viewModel.resetAll() }) {
+            FloatingActionButton(
+                modifier = Modifier.padding(paddingValues = WindowInsets.navigationBars.asPaddingValues()),
+                onClick = { viewModel.resetAll() }
+            ) {
                 Icon(
                     imageVector = Icons.Default.Restore,
                     contentDescription = stringResource(id = R.string.reset)

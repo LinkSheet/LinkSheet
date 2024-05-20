6cb9e183f5abd10d31275ea99f9f237ba2078778
1fexd <PGFvy85nC@protonmail.com>
2024-05-20 23:19:44 +0200

    fix: Preview skip button not working

--- app/src/main/java/fe/linksheet/extension/kotlinx/DeferredExt.kt
+++ app/src/main/java/fe/linksheet/extension/kotlinx/DeferredExt.kt
@@ -1,9 +1,7 @@
 package fe.linksheet.extension.kotlinx
 
 import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.currentCoroutineContext
-import kotlinx.coroutines.ensureActive
 
 suspend fun <T> Deferred<T>.awaitOrNull(): T? {
-    return runCatching { await() }.onFailure { currentCoroutineContext().ensureActive() }.getOrNull()
+    return runCatching { await() }.getOrNull()
 }

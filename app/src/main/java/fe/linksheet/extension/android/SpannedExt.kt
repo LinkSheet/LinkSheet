8ecdaa3bd5a6c8ba4f7cc3298e758cfc0a7f85d3
1fexd <PGFvy85nC@protonmail.com>
2024-06-03 21:46:26 +0200

    chore: Remove dead code

--- app/src/main/java/fe/linksheet/extension/android/SpannedExt.kt
+++
@@ -1,8 +0,0 @@
-package fe.linksheet.extension.android
-
-import android.text.Spanned
-import fe.linksheet.util.spanformatter.SpanFormatter
-
-fun Spanned.format(vararg args: Any?): Spanned {
-    return SpanFormatter.format(this, *args)
-}

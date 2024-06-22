98695e58a46eb445ec09288791b34dd9e66aa251
1fexd <PGFvy85nC@protonmail.com>
2024-06-22 14:55:20 +0200

    chore: Add missing test

---
+++ app/src/test/java/fe/linksheet/PrivateBinPasteServiceTest.kt
@@ -0,0 +1,11 @@
+package fe.linksheet
+
+import org.junit.Test
+
+class PrivateBinPasteServiceTest {
+    @Test
+    fun test(){
+//        val result = PrivateBinPasteService().createPaste("yeet")
+//        result.getOrNull()?.let { println(it.url) }
+    }
+}

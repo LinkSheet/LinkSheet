84abdec95883f5a3c9fadd999c8f9aefed0578a6
1fexd <PGFvy85nC@protonmail.com>
2024-08-24 09:23:00 +0200

    chore: Improve log message

--- app/src/main/java/fe/linksheet/module/analytics/client/DebugLogAnalyticsClient.kt
+++ app/src/main/java/fe/linksheet/module/analytics/client/DebugLogAnalyticsClient.kt
@@ -2,12 +2,10 @@ package fe.linksheet.module.analytics.client
 
 import android.content.Context
 import androidx.compose.ui.util.fastJoinToString
-import androidx.lifecycle.LifecycleCoroutineScope
 import fe.linksheet.extension.koin.createLogger
-import fe.linksheet.extension.koin.single
-import fe.linksheet.module.analytics.*
+import fe.linksheet.module.analytics.AnalyticsClient
+import fe.linksheet.module.analytics.AnalyticsEvent
 import fe.linksheet.module.log.Logger
-import fe.linksheet.module.network.NetworkStateService
 import org.koin.dsl.module
 
 class DebugLogAnalyticsClient(
@@ -22,7 +20,7 @@ class DebugLogAnalyticsClient(
     }
 
     override fun setup(context: Context) {
-        logger.info("Client set up")
+        logger.info("Client set up, using a NoOp-implementation, no events will be sent")
     }
 
     override fun sendEvents(events: List<AnalyticsEvent>): Boolean {

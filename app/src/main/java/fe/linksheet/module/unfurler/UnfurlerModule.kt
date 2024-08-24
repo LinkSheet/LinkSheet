4f80b4941638d4aed91388ce9a6a8bbc5052c073
1fexd <PGFvy85nC@protonmail.com>
2024-08-24 08:01:31 +0200

    fix: Unfurler crash

--- app/src/main/java/fe/linksheet/module/unfurler/UnfurlerModule.kt
+++ app/src/main/java/fe/linksheet/module/unfurler/UnfurlerModule.kt
@@ -1,9 +1,8 @@
 package fe.linksheet.module.unfurler
 
 import me.saket.unfurl.Unfurler
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val unfurlerModule = module {
-    singleOf(::Unfurler)
+    single<Unfurler> { Unfurler() }
 }

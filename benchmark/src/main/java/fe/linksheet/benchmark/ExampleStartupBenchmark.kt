28896f9ce4146a5158faecf5190fe2062e6199a5
1fexd <PGFvy85nC@protonmail.com>
2024-10-11 21:15:55 +0200

    chore: Disable benchmark for now

--- benchmark/src/main/java/fe/linksheet/benchmark/ExampleStartupBenchmark.kt
+++ benchmark/src/main/java/fe/linksheet/benchmark/ExampleStartupBenchmark.kt
@@ -2,8 +2,8 @@ package fe.linksheet.benchmark
 
 import android.content.Intent
 import android.net.Uri
-import androidx.benchmark.macro.StartupTimingMetric
-import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+//import androidx.benchmark.macro.StartupTimingMetric
+//import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import org.junit.Rule
@@ -13,18 +13,18 @@ import org.junit.runner.RunWith
 @LargeTest
 @RunWith(AndroidJUnit4::class)
 class SampleStartupBenchmark {
-    @get:Rule
-    val benchmarkRule = MacrobenchmarkRule()
-
-    @Test
-    fun startup() = benchmarkRule.measureRepeated(
-        packageName = "fe.linksheet",
-        metrics = listOf(StartupTimingMetric()),
-        iterations = 5,
-        setupBlock = {
-            pressHome()
-        }
-    ) {
-        startActivityAndWait(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=evIpx9Onc2c")))
-    }
+//    @get:Rule
+//    val benchmarkRule = MacrobenchmarkRule()
+//
+//    @Test
+//    fun startup() = benchmarkRule.measureRepeated(
+//        packageName = "fe.linksheet",
+//        metrics = listOf(StartupTimingMetric()),
+//        iterations = 5,
+//        setupBlock = {
+//            pressHome()
+//        }
+//    ) {
+//        startActivityAndWait(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=evIpx9Onc2c")))
+//    }
 }

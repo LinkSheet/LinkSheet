a42fd12c5ec33920955378e4436bab6fe2d0d25f
1fexd <PGFvy85nC@protonmail.com>
2024-10-15 05:52:17 +0200

    fix: Memory leak?

--- app/src/main/java/fe/linksheet/extension/compose/ClipboardManagerExt.kt
+++ app/src/main/java/fe/linksheet/extension/compose/ClipboardManagerExt.kt
@@ -6,12 +6,13 @@ import androidx.compose.ui.platform.ClipboardManager
 @Composable
 fun ClipboardManager.observeAsState(): State<String?> {
     val clipboardText = remember { mutableStateOf(getText()?.text) }
-
-    DisposableEffect(nativeClipboard) {
-        val listener = android.content.ClipboardManager.OnPrimaryClipChangedListener {
+    val listener = remember(nativeClipboard) {
+        android.content.ClipboardManager.OnPrimaryClipChangedListener {
             clipboardText.value = getText()?.text
         }
+    }
 
+    DisposableEffect(nativeClipboard) {
         nativeClipboard.addPrimaryClipChangedListener(listener)
         onDispose {
             nativeClipboard.removePrimaryClipChangedListener(listener)

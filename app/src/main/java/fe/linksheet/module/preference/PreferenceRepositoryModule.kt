7a71300fd36db1ac6f4e47554c5a6eee6e0c3220
1fexd <PGFvy85nC@protonmail.com>
2024-10-26 17:16:30 +0200

    chore: Prepare new preference repository

--- app/src/main/java/fe/linksheet/module/preference/PreferenceRepositoryModule.kt
+++ app/src/main/java/fe/linksheet/module/preference/PreferenceRepositoryModule.kt
@@ -3,6 +3,7 @@ package fe.linksheet.module.preference
 import fe.linksheet.module.preference.app.AppPreferenceRepository
 import fe.linksheet.module.preference.experiment.ExperimentRepository
 import fe.linksheet.module.preference.flags.FeatureFlagRepository
+import fe.linksheet.module.preference.state.AppStateRepository
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
@@ -10,4 +11,5 @@ val preferenceRepositoryModule = module {
     singleOf(::AppPreferenceRepository)
     singleOf(::FeatureFlagRepository)
     singleOf(::ExperimentRepository)
+    singleOf(::AppStateRepository)
 }
---
+++ app/src/main/java/fe/linksheet/module/preference/state/AppStatePreferences.kt
@@ -0,0 +1,9 @@
+package fe.linksheet.module.preference.state
+
+import fe.android.preference.helper.PreferenceDefinition
+
+object AppStatePreferences : PreferenceDefinition() {
+    init {
+        finalize()
+    }
+}

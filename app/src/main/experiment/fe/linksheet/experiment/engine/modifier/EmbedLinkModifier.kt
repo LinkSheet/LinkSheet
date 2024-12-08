ca8fbe5aae4e8105eb1d42ae9e65bfdb3c4e8aaa
1fexd <PGFvy85nC@protonmail.com>
2024-12-08 18:40:31 +0100

    wip

--- app/src/main/experiment/fe/linksheet/experiment/engine/modifier/EmbedLinkModifier.kt
+++ app/src/main/experiment/fe/linksheet/experiment/engine/modifier/EmbedLinkModifier.kt
@@ -2,17 +2,17 @@ package fe.linksheet.experiment.engine.modifier
 
 import fe.embed.resolve.EmbedResolver
 import fe.embed.resolve.config.ConfigType
-import fe.linksheet.experiment.engine.Input
+import fe.linksheet.experiment.engine.ModifyInput
 import fe.linksheet.experiment.engine.LinkModifier
-import fe.linksheet.experiment.engine.Output
+import fe.linksheet.experiment.engine.ModifyOutput
 
 class EmbedLinkModifier : LinkModifier {
     companion object {
         private val embedResolverBundled by lazy { ConfigType.Bundled.load() }
     }
 
-    override fun modify(data: Input): Output? {
+    override suspend fun modify(data: ModifyInput): ModifyOutput? {
         val result = EmbedResolver.resolve(data.url, embedResolverBundled)
-        return result?.let { Output(it) }
+        return result?.let { ModifyOutput(it) }
     }
 }

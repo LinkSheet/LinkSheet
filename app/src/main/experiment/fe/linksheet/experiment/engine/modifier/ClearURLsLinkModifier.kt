00d880c0006b7c0ff596a3c84997d81a73606460
1fexd <PGFvy85nC@protonmail.com>
2025-01-29 06:40:49 +0100

    fixup!: Rebase conflicts

--- app/src/main/experiment/fe/linksheet/experiment/engine/modifier/ClearURLsLinkModifier.kt
+++ app/src/main/experiment/fe/linksheet/experiment/engine/modifier/ClearURLsLinkModifier.kt
@@ -1,18 +1,20 @@
 package fe.linksheet.experiment.engine.modifier
 
-import fe.clearurlskt.ClearURL
-import fe.clearurlskt.ClearURLLoader
-import fe.linksheet.experiment.engine.ModifyInput
+import fe.clearurlskt.ClearUrls
+import fe.clearurlskt.loader.BundledClearURLConfigLoader
 import fe.linksheet.experiment.engine.LinkModifier
+import fe.linksheet.experiment.engine.ModifyInput
 import fe.linksheet.experiment.engine.ModifyOutput
 
 class ClearURLsLinkModifier : LinkModifier {
     companion object {
-        private val clearUrlProviders by lazy { ClearURLLoader.loadBuiltInClearURLProviders() }
+        private val clearUrlProviders by lazy { BundledClearURLConfigLoader.load().getOrNull() }
     }
 
-    override suspend fun modify(data: ModifyInput): ModifyOutput {
-        val result = ClearURL.clearUrl(data.url, clearUrlProviders)
-        return ModifyOutput(result)
+    private val clearUrls = clearUrlProviders?.let { ClearUrls(it) }
+
+    override suspend fun modify(data: ModifyInput): ModifyOutput? {
+        val result = clearUrls?.clearUrl(data.url)?.first
+        return result?.let { ModifyOutput(it) }
     }
 }

67600b0c264a24ba7b76ee8480a1db2ea77fbd40
1fexd <PGFvy85nC@protonmail.com>
2025-01-29 06:40:49 +0100

    fixup!: Rebase conflicts

--- app/src/main/experiment/fe/linksheet/experiment/engine/modifier/EmbedLinkModifier.kt
+++ app/src/main/experiment/fe/linksheet/experiment/engine/modifier/EmbedLinkModifier.kt
@@ -1,18 +1,20 @@
 package fe.linksheet.experiment.engine.modifier
 
 import fe.embed.resolve.EmbedResolver
-import fe.embed.resolve.config.ConfigType
-import fe.linksheet.experiment.engine.ModifyInput
+import fe.embed.resolve.loader.BundledEmbedResolveConfigLoader
 import fe.linksheet.experiment.engine.LinkModifier
+import fe.linksheet.experiment.engine.ModifyInput
 import fe.linksheet.experiment.engine.ModifyOutput
 
 class EmbedLinkModifier : LinkModifier {
     companion object {
-        private val embedResolverBundled by lazy { ConfigType.Bundled.load() }
+        private val embedResolverConfig by lazy { BundledEmbedResolveConfigLoader.load().getOrNull() }
     }
 
+    private val embedResolver = embedResolverConfig?.let { EmbedResolver(it) }
+
     override suspend fun modify(data: ModifyInput): ModifyOutput? {
-        val result = EmbedResolver.resolve(data.url, embedResolverBundled)
+        val result = embedResolver?.resolve(data.url)
         return result?.let { ModifyOutput(it) }
     }
 }

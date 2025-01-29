aa832e1e4c71dfcf9cbaadd88be80454a017b5c2
1fexd <PGFvy85nC@protonmail.com>
2025-01-29 07:44:20 +0100

    feat: Add refresh header parsing

--- app/src/main/experiment/fe/linksheet/experiment/engine/resolver/redirects/FollowRedirectsLinkResolverSource.kt
+++ app/src/main/experiment/fe/linksheet/experiment/engine/resolver/redirects/FollowRedirectsLinkResolverSource.kt
@@ -2,6 +2,7 @@ package fe.linksheet.experiment.engine.resolver.redirects
 
 import fe.linksheet.experiment.engine.DataSource
 import fe.linksheet.module.resolver.urlresolver.ResolveResultType
+import fe.linksheet.module.resolver.urlresolver.redirect.RedirectResolveRequest.Companion.parseRefreshHeader
 import fe.std.result.*
 import io.ktor.client.*
 import io.ktor.client.request.*
@@ -21,6 +22,11 @@ suspend fun resolveLocal(client: HttpClient, url: Url): IResult<ResolveResultTyp
     }
 
     val headResponse by headResult
+    val refreshHeaderUrl = headResponse.handleRefreshHeader()
+    if (refreshHeaderUrl != null) {
+        return Success(ResolveResultType.Resolved.Local(refreshHeaderUrl))
+    }
+
     if (headResponse.status.value !in 400..499) {
         return headResponse.toSuccessResult()
     }
@@ -30,6 +36,16 @@ suspend fun resolveLocal(client: HttpClient, url: Url): IResult<ResolveResultTyp
         true -> getResult.value.toSuccessResult()
         false -> getResult.cast()
     }
+}
+
+private fun HttpResponse.handleRefreshHeader(): String? {
+    val refreshHeader = headers["refresh"] ?: return null
+    val parsedHeader = parseRefreshHeader(refreshHeader) ?: return null
+
+    return parsedHeader
+        .takeIf { it.first == 0 }
+        ?.takeIf { parseUrl(it.second) != null }
+        ?.second
 }
 
 private fun HttpResponse.toSuccessResult(): IResult<ResolveResultType> {

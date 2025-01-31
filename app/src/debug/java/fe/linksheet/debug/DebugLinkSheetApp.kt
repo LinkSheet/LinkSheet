3f4f95e7c3fb3c517c143dab8786004a9292099b
1fexd <PGFvy85nC@protonmail.com>
2025-01-31 18:17:33 +0100

    fix: Tests in CI

--- app/src/debug/java/fe/linksheet/debug/DebugLinkSheetApp.kt
+++ app/src/debug/java/fe/linksheet/debug/DebugLinkSheetApp.kt
@@ -4,11 +4,13 @@ import fe.linksheet.LinkSheetApp
 import fe.linksheet.debug.module.debug.RealDebugMenuSlotProvider
 import fe.linksheet.debug.module.devicecompat.DebugMiuiCompatProvider
 import fe.linksheet.debug.module.viewmodel.module.DebugViewModelModule
+import fe.linksheet.module.analytics.client.DebugLogAnalyticsClient
 import fe.linksheet.module.analytics.client.aptabaseAnalyticsClientModule
 import fe.linksheet.module.debug.DebugMenuSlotProvider
 import fe.linksheet.module.devicecompat.miui.MiuiCompatProvider
 import fe.linksheet.module.devicecompat.samsung.RealSamsungIntentCompatProvider
 import fe.linksheet.module.devicecompat.samsung.SamsungIntentCompatProvider
+import fe.linksheet.util.buildconfig.BuildType
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
@@ -24,8 +26,9 @@ class DebugLinkSheetApp : LinkSheetApp() {
         }
     }
 
-    override fun provideAnalyticsClient(): Module {
-        return aptabaseAnalyticsClientModule
+    override fun provideAnalyticsClient(): Module = when {
+        BuildType.current.isTestRunner -> DebugLogAnalyticsClient.module
+        else -> aptabaseAnalyticsClientModule
     }
 
     override fun provideDebugMenu(): Module {

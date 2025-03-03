6d07770fcf521377ad553bf96063f9393b643641
1fexd <PGFvy85nC@protonmail.com>
2025-03-03 21:27:41 +0100

    fix: Build

--- .github/workflows/build-nightly.main.kts
+++ .github/workflows/build-nightly.main.kts
@@ -276,7 +276,7 @@ workflow(
     ),
     on = listOf(
         WorkflowDispatch(), Push(
-            tags = listOf("nightly/(2[0-9][2-9][4-9])((0[1-9])|11|12)([0-3][0-9])([0-9][0-9])"),
+            tags = listOf("nightly-*"),
             pathsIgnore = listOf("*.md")
         )
     ),

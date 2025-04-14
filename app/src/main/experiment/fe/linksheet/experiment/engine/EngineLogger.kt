042100ee9e48990e9d27df39d385197a406f5049
1fexd <PGFvy85nC@protonmail.com>
2025-04-14 16:38:38 +0200

    feat: Implement rule mvp, refactoring

---
+++ app/src/main/experiment/fe/linksheet/experiment/engine/EngineLogger.kt
@@ -0,0 +1,13 @@
+package fe.linksheet.experiment.engine
+
+import android.util.Log
+
+abstract class EngineLogger(protected val tag: String) {
+    abstract fun debug(message: () -> String)
+}
+
+class LogcatEngineLogger(tag: String) : EngineLogger(tag) {
+    override fun debug(message: () -> String) {
+        Log.d(tag, message())
+    }
+}

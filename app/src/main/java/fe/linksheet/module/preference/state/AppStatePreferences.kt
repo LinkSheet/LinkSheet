ab28b51da41047c7036e2ec955fe7b4728069e81
1fexd <PGFvy85nC@protonmail.com>
2025-04-24 07:49:28 +0200

    chore: Disable state

--- app/src/main/java/fe/linksheet/module/preference/state/AppStatePreferences.kt
+++ app/src/main/java/fe/linksheet/module/preference/state/AppStatePreferences.kt
@@ -9,7 +9,7 @@ object AppStatePreferences : PreferenceDefinition() {
     val newDefaults_2024_12_16 = long("has_new_defaults_2024_12_16")
     val newDefaults_2024_12_16_InfoDismissed = boolean("has_new_defaults_2024_12_16_info_dismissed")
 
-    val newDefaults_2024_12_29_InfoDismissed = boolean("has_new_defaults_2024_12_29_info_dismissed")
+    val newDefaults_2024_12_29_InfoDismissed = boolean("has_new_defaults_2024_12_29_info_dismissed", true)
 
     init {
         finalize()

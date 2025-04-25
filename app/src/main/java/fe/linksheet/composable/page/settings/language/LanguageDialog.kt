e4b9cbda85aed64d372644111fadcba6d23774a3
1fexd <PGFvy85nC@protonmail.com>
2025-04-25 14:22:25 +0200

    chore: Use composekit alert dialog

--- app/src/main/java/fe/linksheet/composable/page/settings/language/LanguageDialog.kt
+++ app/src/main/java/fe/linksheet/composable/page/settings/language/LanguageDialog.kt
@@ -1,14 +1,12 @@
 package fe.linksheet.composable.page.settings.language
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -22,15 +20,12 @@ import fe.android.compose.dialog.helper.result.rememberResultDialogState
 import fe.android.compose.feedback.FeedbackType
 import fe.android.compose.feedback.LocalHapticFeedbackInteraction
 import fe.android.compose.feedback.wrap
+import fe.android.compose.text.ComposableTextContent.Companion.content
 import fe.android.compose.text.DefaultContent.Companion.text
 import fe.android.compose.text.StringResourceContent.Companion.textContent
-import fe.composekit.component.dialog.AlertDialog
 import fe.composekit.component.dialog.DialogDefaults
-import fe.composekit.component.dialog.DialogDefaults.DialogBoxPadding
-import fe.composekit.component.dialog.DialogDefaults.DialogIconBottomPadding
-import fe.composekit.component.dialog.DialogDefaults.DialogTextBottomPadding
-import fe.composekit.component.dialog.DialogDefaults.DialogTitleBottomPadding
-import fe.composekit.component.dialog.DialogPaddingOptions
+import fe.composekit.component.dialog.SaneAlertDialog
+import fe.composekit.component.dialog.SaneAlertDialogTextButton
 import fe.composekit.component.list.item.ContentPosition
 import fe.composekit.component.list.item.type.RadioButtonListItem
 import fe.linksheet.R
@@ -38,8 +33,6 @@ import fe.linksheet.composable.ui.DialogTitleStyle
 import fe.linksheet.module.language.DisplayLocaleItem
 import fe.linksheet.module.language.LocaleItem
 import fe.linksheet.module.viewmodel.LanguageSettingsViewModel
-import my.nanihadesuka.compose.LazyColumnScrollbar
-import my.nanihadesuka.compose.ScrollbarSettings
 import org.koin.compose.viewmodel.koinViewModel
 import java.util.*
 
@@ -102,88 +95,50 @@ private fun LanguageDialog(
     var selectedLocale by remember { mutableStateOf(currentLocale) }
 
     val state = rememberLazyListState()
-    AlertDialog(
-        padding = DialogPaddingOptions(
-            box = PaddingValues(vertical = DialogBoxPadding),
-            icon = PaddingValues(
-                start = DialogBoxPadding,
-                end = DialogBoxPadding,
-                bottom = DialogIconBottomPadding
-            ),
-            title = PaddingValues(
-                start = DialogBoxPadding,
-                end = DialogBoxPadding,
-                bottom = DialogTitleBottomPadding
-            ),
-            text = PaddingValues(
-                bottom = DialogTextBottomPadding
-            ),
-            buttons = PaddingValues(horizontal = DialogBoxPadding)
-        ),
-        title = {
+    SaneAlertDialog(
+        state = state,
+        title = content {
             Text(
                 text = stringResource(id = R.string.settings_language__dialog_title),
                 style = DialogTitleStyle,
             )
         },
-        text = {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(max = 280.dp),
-//                verticalArrangement = Arrangement.spacedBy(5.dp)
-            ) {
-                HorizontalDivider()
-
-                LazyColumnScrollbar(
-                    modifier = Modifier,
-                    state = state,
-                    settings = ScrollbarSettings(
-                        thumbThickness = 3.dp,
-                        scrollbarPadding = 0.dp,
-                        alwaysShowScrollbar = true,
-                        thumbSelectedColor = MaterialTheme.colorScheme.primary,
-                        thumbUnselectedColor = MaterialTheme.colorScheme.primary
-                    )
-                ) {
-                    LazyColumn(
-                        modifier = Modifier
-                            .selectableGroup()
-                            .padding(horizontal = 16.dp),
-                        state = state,
-                        contentPadding = PaddingValues(vertical = 4.dp),
-                        verticalArrangement = Arrangement.spacedBy(2.dp)
-                    ) {
-                        items(items = locales, key = { it.item.locale.hashCode() }) { displayItem ->
-                            LanguageListItem(
-                                displayItem = displayItem,
-                                isDeviceLanguage = displayItem.isDeviceLanguage,
-                                selected = (selectedLocale == displayItem.item) || (selectedLocale?.locale == deviceLocale && displayItem.isDeviceLanguage),
-                                onSelect = {
-                                    selectedLocale = displayItem.item
-                                }
-                            )
-                        }
-                    }
-                }
-
-                HorizontalDivider()
-            }
-        },
-        onDismissRequest = onDismiss,
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text(text = stringResource(id = R.string.cancel))
-            }
-        },
+        onDismiss = {},
         confirmButton = {
-            TextButton(onClick = {
-                selectedLocale?.let { onConfirm(it) }
-            }) {
-                Text(text = stringResource(id = R.string.save))
+            SaneAlertDialogTextButton(
+                content = textContent(R.string.save),
+                onClick = {
+                    selectedLocale?.let { onConfirm(it) }
+                }
+            )
+        },
+        dismissButton = {
+            SaneAlertDialogTextButton(
+                content = textContent(R.string.cancel),
+                onClick = onDismiss
+            )
+        },
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .selectableGroup()
+                .matchParentSize(),
+            state = state,
+            contentPadding = PaddingValues(vertical = 4.dp),
+            verticalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
+            items(items = locales, key = { it.item.locale.hashCode() }) { displayItem ->
+                LanguageListItem(
+                    displayItem = displayItem,
+                    isDeviceLanguage = displayItem.isDeviceLanguage,
+                    selected = (selectedLocale == displayItem.item) || (selectedLocale?.locale == deviceLocale && displayItem.isDeviceLanguage),
+                    onSelect = {
+                        selectedLocale = displayItem.item
+                    }
+                )
             }
         }
-    )
+    }
 }
 
 @Composable

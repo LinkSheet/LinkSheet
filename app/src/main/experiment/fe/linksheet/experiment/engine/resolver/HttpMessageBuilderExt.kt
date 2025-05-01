4f2b2ebd63874d86562b5018230424867dda2c46
1fexd <PGFvy85nC@protonmail.com>
2025-05-01 15:54:28 +0200

    chore: Cleanup

--- app/src/main/experiment/fe/linksheet/experiment/engine/resolver/HttpMessageBuilderExt.kt
+++ app/src/main/experiment/fe/linksheet/experiment/engine/resolver/HttpMessageBuilderExt.kt
@@ -1,6 +1,7 @@
 package fe.linksheet.experiment.engine.resolver
 
 import io.ktor.client.request.header
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMessageBuilder
 import me.saket.unfurl.extension.HtmlMetadataUnfurlerExtension
 
@@ -8,8 +9,8 @@ fun HttpMessageBuilder.configureHeaders(
     httpUserAgent: String = HtmlMetadataUnfurlerExtension.SlackBotUserAgent,
     htmlByteLimit: Long = 32_768
 ) {
-    header("User-Agent", httpUserAgent)
-    header("Accept", "text/html")
-    header("Accept-Language", "en-US,en;q=0.5")
-    header("Range", "bytes=0-$htmlByteLimit")
+    header(HttpHeaders.UserAgent, httpUserAgent)
+    header(HttpHeaders.Accept, "text/html")
+    header(HttpHeaders.AcceptLanguage, "en-US,en;q=0.5")
+    header(HttpHeaders.Range, "bytes=0-$htmlByteLimit")
 }

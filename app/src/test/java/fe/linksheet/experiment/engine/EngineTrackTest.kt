f80aa229c4d5e65b87ad8c46a3ca2f71a3402ae0
1fexd <PGFvy85nC@protonmail.com>
2025-05-03 15:20:55 +0200

    feat: Implement tracks and fetching in engine

---
+++ app/src/test/java/fe/linksheet/experiment/engine/EngineTrackTest.kt
@@ -0,0 +1,50 @@
+@file:OptIn(ExperimentalUuidApi::class)
+
+package fe.linksheet.experiment.engine
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotNull
+import assertk.assertions.prop
+import fe.linksheet.experiment.engine.rule.BaseRuleEngineTest
+import fe.linksheet.experiment.engine.rule.EmptyLinkEngine
+import fe.std.uri.toStdUrlOrThrow
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import org.junit.runner.RunWith
+import kotlin.test.Test
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@RunWith(AndroidJUnit4::class)
+internal class EngineTrackTest : BaseRuleEngineTest() {
+    private val dispatcher = StandardTestDispatcher()
+    private val testPrintLogger = createTestEngineLogger<EngineTrackTest>()
+
+    @Test
+    fun test() = runTest(dispatcher) {
+        val track1 = EngineTrack(
+            id = Uuid.random(),
+            position = 0,
+            predicate = { false },
+            engine = EmptyLinkEngine
+        )
+
+        val track2 = EngineTrack(
+            id = Uuid.NIL,
+            position = 1,
+            predicate = { it.url.host == "linksheet.app" },
+            engine = EmptyLinkEngine
+        )
+
+        val selector = TrackSelector(listOf(track1, track2))
+        val input = Input("https://linksheet.app/test".toStdUrlOrThrow(), null)
+        val track = selector.find(input)
+
+        assertThat(track)
+            .isNotNull()
+            .prop(EngineTrack::id)
+            .isEqualTo(Uuid.NIL)
+    }
+}

f36751d273bdf56577039ca3c1f8f9eb6b9101f3
1fexd <PGFvy85nC@protonmail.com>
2025-05-05 08:07:42 +0200

    chore: Refactoring

--- app/src/main/java/fe/linksheet/module/resolver/util/CustomTabHandler.kt
+++ app/src/main/java/fe/linksheet/module/resolver/util/CustomTabHandler.kt
@@ -18,6 +18,33 @@ object CustomTabHandler {
 
         return CustomTabInfo(true, dropExtras)
     }
+
+    fun getInfo2(intent: SafeIntent, allowCustomTab: Boolean): CustomTabInfo2 {
+        val isCustomTab = intent.hasExtra(EXTRA_SESSION)
+        if (!isCustomTab) {
+            return CustomTabInfo2.NotApplicable
+        }
+
+        if (!allowCustomTab) {
+            return CustomTabInfo2.NotAllowed
+        }
+
+        val dropExtras = intent.extras
+            ?.keySet()
+            ?.filter { extra -> !extra.contains(EXTRA_INDICATOR) }
+            ?: emptyList()
+
+        return CustomTabInfo2.Allowed(dropExtras)
+    }
 }
 
 data class CustomTabInfo(val isCustomTab: Boolean, val extras: List<String>?)
+
+sealed interface CustomTabInfo2 {
+    val dropExtras: List<String>
+        get() = emptyList()
+
+    data object NotApplicable : CustomTabInfo2
+    data object NotAllowed : CustomTabInfo2
+    data class Allowed(override val dropExtras: List<String> ) : CustomTabInfo2
+}

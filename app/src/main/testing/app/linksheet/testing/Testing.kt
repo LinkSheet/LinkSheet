af240ca64301f85afc543c4f0937738a199a10b4
1fexd <PGFvy85nC@protonmail.com>
2025-05-28 18:22:40 +0200

    fix: Check for CI

--- app/src/main/testing/app/linksheet/testing/Testing.kt
+++ app/src/main/testing/app/linksheet/testing/Testing.kt
@@ -1,7 +1,8 @@
 package app.linksheet.testing
 
 import android.os.Build
+import fe.linksheet.BuildConfig
 
 object Testing {
-    val IsTestRunner by lazy { Build.FINGERPRINT.equals("robolectric", ignoreCase = true) }
+    val IsTestRunner by lazy { Build.FINGERPRINT.equals("robolectric", ignoreCase = true) || BuildConfig.IS_CI }
 }

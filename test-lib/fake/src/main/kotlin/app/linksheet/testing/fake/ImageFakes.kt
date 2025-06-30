40ece1911ecf2a59a46f3da603d107b7c5450ee2
1fexd <PGFvy85nC@protonmail.com>
2025-06-30 18:38:12 +0200

    fix: Test

--- test-lib/fake/src/main/kotlin/app/linksheet/testing/fake/ImageFakes.kt
+++ test-lib/fake/src/main/kotlin/app/linksheet/testing/fake/ImageFakes.kt
@@ -5,6 +5,9 @@ import android.graphics.drawable.shapes.RectShape
 import androidx.compose.ui.graphics.ImageBitmap
 
 object ImageFakes {
-    val EmptyDrawable = ShapeDrawable(RectShape())
+    val EmptyDrawable = ShapeDrawable(RectShape()).apply {
+        intrinsicWidth = 1
+        intrinsicHeight = 1
+    }
     val ImageBitmap = lazy { ImageBitmap(1, 1) }
 }

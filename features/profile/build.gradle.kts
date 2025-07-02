5dab331b25815f2fd3b6397d216912f117e0541c
1fexd <PGFvy85nC@protonmail.com>
2025-07-02 22:05:30 +0200

    fix: Profile switcher

--- features/profile/build.gradle.kts
+++ features/profile/build.gradle.kts
@@ -1,6 +1,10 @@
 import com.gitlab.grrfe.gradlebuild.android.AndroidSdk
 import fe.build.dependencies._1fexd
 import fe.buildlogic.Version
+import fe.buildlogic.common.CompilerOption
+import fe.buildlogic.common.PluginOption
+import fe.buildlogic.common.extension.addCompilerOptions
+import fe.buildlogic.common.extension.addPluginOptions
 
 plugins {
     id("com.android.library")
@@ -28,7 +32,7 @@ android {
 }
 
 dependencies {
-    api(project(":hidden-api"))
+    compileOnly(project(":hidden-api"))
     implementation(project(":util"))
     implementation(platform(_1fexd.composeKit.bom))
     implementation(_1fexd.composeKit.core)

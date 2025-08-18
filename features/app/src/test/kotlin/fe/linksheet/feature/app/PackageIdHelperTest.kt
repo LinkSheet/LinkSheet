2ff229026d757ffb8a3cc097e8f2baa6f9d0565c
1fexd <PGFvy85nC@protonmail.com>
2025-08-18 06:58:39 +0200

    chore: Import

--- features/app/src/test/kotlin/fe/linksheet/feature/app/PackageIdHelperTest.kt
+++ features/app/src/test/kotlin/fe/linksheet/feature/app/PackageIdHelperTest.kt
@@ -7,7 +7,6 @@ import app.linksheet.testing.fake.asDescriptors
 import assertk.assertThat
 import assertk.assertions.containsExactly
 import fe.linksheet.testlib.core.BaseUnitTest
-import fe.linksheet.testlib.core.JunitTest
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 

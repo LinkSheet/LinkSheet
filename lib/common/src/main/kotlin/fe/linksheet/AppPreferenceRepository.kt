b6ea3b695db0366f1248413c70a1bad64248b564
1fexd <PGFvy85nC@protonmail.com>
2025-10-14 08:27:53 +0200

    chore: Dependencies

---
+++ lib/common/src/main/kotlin/fe/linksheet/AppPreferenceRepository.kt
@@ -0,0 +1,7 @@
+package fe.linksheet
+
+import android.content.Context
+import fe.composekit.preference.FlowPreferenceRepository
+
+abstract class AppPreferenceRepository(context: Context) : FlowPreferenceRepository(context) {
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,9 +20,7 @@ pluginManagement {
 
     plugins {
         id("de.fayard.refreshVersions") version "0.60.6"
-        id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
-////                                                       # available:"1.0.0-rc-1"
-////                                                       # available:"1.0.0"
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
         id("com.android.library")
         id("org.jetbrains.kotlin.android")
         id("net.nemerosa.versioning")

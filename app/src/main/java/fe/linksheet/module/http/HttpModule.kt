b218848c1680ef03312f453e0eb21bcd47d25acf
1fexd <PGFvy85nC@protonmail.com>
2026-01-04 15:58:06 +0100

    fix: OkHttp traffic tagging

--- app/src/main/java/fe/linksheet/module/http/HttpModule.kt
+++ app/src/main/java/fe/linksheet/module/http/HttpModule.kt
@@ -1,23 +1,23 @@
 package fe.linksheet.module.http
 
 import android.content.Context
+import android.net.TrafficStats
 import app.linksheet.api.CachedRequest
 import coil3.ImageLoader
 import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 import fe.httpkt.Request
 import fe.linksheet.module.resolver.urlresolver.RealCachedRequest
 import fe.linksheet.util.buildconfig.Build
-import fe.linksheet.util.withStatsTag
 import io.ktor.client.*
-import io.ktor.client.engine.okhttp.*
+import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.gson.*
 import me.saket.unfurl.Unfurler
-import okhttp3.Cache
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
+import okhttp3.*
 import org.koin.dsl.module
+import java.net.InetSocketAddress
+import java.net.Proxy
 
 val HttpModule = module {
     // TODO: Do we actually need this as a DI singleton?
@@ -29,10 +29,25 @@ val HttpModule = module {
         val cache = Cache(context.cacheDir, 25 * 1024 * 1024)
 
         OkHttpClient.Builder()
-            .addInterceptor(Interceptor { chain ->
-                // https://github.com/jaredsburrows/android-gif-search/pull/525
-                withStatsTag(0xF00D) { chain.proceed(chain.request()) }
+            // Via https://github.com/jaredsburrows/android-gif-search/blob/19ea35435e0962cd7d419a4ee02b05f5cebdb6e6/app/src/main/java/com/burrowsapps/gif/search/di/NetworkModule.kt#L90
+            .eventListener(eventListener = object : EventListener() {
+                override fun connectStart(call: Call, inetSocketAddress: InetSocketAddress, proxy: Proxy) {
+                    TrafficStats.setThreadStatsTag(0xF00D)
+                }
+
+                override fun connectEnd(
+                    call: Call,
+                    inetSocketAddress: InetSocketAddress,
+                    proxy: Proxy,
+                    protocol: Protocol?
+                ) {
+                    TrafficStats.clearThreadStatsTag()
+                }
             })
+//            .addInterceptor(Interceptor { chain ->
+//                // https://github.com/jaredsburrows/android-gif-search/pull/525
+//                withStatsTag(0xF00D) { chain.proceed(chain.request()) }
+//            })
             .cache(cache)
             // Default
 //            .followRedirects(true)

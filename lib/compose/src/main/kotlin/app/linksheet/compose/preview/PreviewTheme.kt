7db4f2699c69b788b92505499f1923e4424d6c13
1fexd <PGFvy85nC@protonmail.com>
2026-01-13 06:45:46 +0100

    chore: Add LocalHapticFeedbackInteraction

--- lib/compose/src/main/kotlin/app/linksheet/compose/preview/PreviewTheme.kt
+++ lib/compose/src/main/kotlin/app/linksheet/compose/preview/PreviewTheme.kt
@@ -2,14 +2,19 @@ package app.linksheet.compose.preview
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import app.linksheet.compose.theme.LightColors
 import app.linksheet.compose.theme.NewTypography
+import fe.android.compose.feedback.LocalHapticFeedbackInteraction
+import fe.android.compose.feedback.PreviewHapticFeedbackInteraction
 
 @Composable
 fun PreviewTheme(content: @Composable () -> Unit) {
-    MaterialTheme(
-        colorScheme = LightColors,
-        typography = NewTypography,
-        content = content
-    )
+    CompositionLocalProvider(LocalHapticFeedbackInteraction provides PreviewHapticFeedbackInteraction) {
+        MaterialTheme(
+            colorScheme = LightColors,
+            typography = NewTypography,
+            content = content
+        )
+    }
 }

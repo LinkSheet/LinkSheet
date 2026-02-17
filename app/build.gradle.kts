13363fb6b508334c1f272d93279ec304466a3daa
1fexd <PGFvy85nC@protonmail.com>
2026-02-17 07:58:47 +0100

    Revert "fix: Build"
    This reverts commit 6fd11c9cef096ad5ff5b633a3fdfa18c24564f94.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -423,6 +423,7 @@ dependencies {
     implementation("org.lsposed.hiddenapibypass:hiddenapibypass:_")
     implementation("dev.rikka.tools.refine:runtime:_")
 
+    implementation(MozillaComponents.support.utils)
     implementation(MozillaComponents.lib.publicSuffixList)
     implementation(platform(KotlinX.serialization.bom))
     implementation(KotlinX.serialization.json)

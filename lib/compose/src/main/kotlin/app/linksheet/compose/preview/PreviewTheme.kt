0e666156ca3b0dcc836748fabf7524bc5a5d4180
1fexd <PGFvy85nC@protonmail.com>
2026-02-23 07:44:10 +0100

    fix: Respect system dark mode flag in composable preview theme

--- lib/compose/src/main/kotlin/app/linksheet/compose/preview/PreviewTheme.kt
+++ lib/compose/src/main/kotlin/app/linksheet/compose/preview/PreviewTheme.kt
@@ -1,8 +1,10 @@
 package app.linksheet.compose.preview
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import app.linksheet.compose.theme.DarkColors
 import app.linksheet.compose.theme.LightColors
 import app.linksheet.compose.theme.NewTypography
 import fe.android.compose.feedback.LocalHapticFeedbackInteraction
@@ -12,7 +14,7 @@ import fe.android.compose.feedback.PreviewHapticFeedbackInteraction
 fun PreviewTheme(content: @Composable () -> Unit) {
     CompositionLocalProvider(LocalHapticFeedbackInteraction provides PreviewHapticFeedbackInteraction) {
         MaterialTheme(
-            colorScheme = LightColors,
+            colorScheme = if(isSystemInDarkTheme()) DarkColors else LightColors,
             typography = NewTypography,
             content = content
         )

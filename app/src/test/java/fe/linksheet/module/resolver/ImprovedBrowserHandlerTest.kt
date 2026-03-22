LinkSheet/LinkSheet

commit 567753d29ef5c06ea81b5a829892fa6df26820d2

#628 fix: Properly handle selected browser

app/src/main/java/fe/linksheet/module/resolver/ImprovedBrowserHandler.kt
@@ -2,7 +2,6 @@ package fe.linksheet.module.resolver
 
 import android.content.pm.ResolveInfo
 import fe.linksheet.extension.android.activityDescriptor
-import fe.linksheet.util.extension.android.componentName
 
 class ImprovedBrowserHandler(
     private val autoLaunchSingleBrowserExperiment: () -> Boolean
@@ -39,19 +38,19 @@ class ImprovedBrowserHandler(
 
             is BrowserModeConfigHelper.SelectedBrowser -> {
                 val browserResolveInfo = browsers[config.selectedBrowser]
-                val list = if (browserResolveInfo == null) emptyList() else listOf(browserResolveInfo)
+                val hasInfo = browserResolveInfo != null
+                val list = if (hasInfo) listOf(browserResolveInfo) else emptyList()
 
-                // TODO: Need to use merged here since resolvedList might contain ResolveInfos also present in browsers
-                // TODO: Do we really need to use the component?
-                val isSingleOption = nonBrowsers.isEmpty()
-                        && browsers.size == 1
-                        && browsers.values.singleOrNull()?.activityInfo?.componentName == browserResolveInfo?.activityInfo?.componentName
-
+//                // TODO: Need to use merged here since resolvedList might contain ResolveInfos also present in browsers
+//                // TODO: Do we really need to use the component?
+//                val isSingleOption = nonBrowsers.isEmpty()
+//                        && browsers.size == 1
+//                        && browsers.values.singleOrNull()?.activityInfo?.componentName == browserResolveInfo?.activityInfo?.componentName
                 FilteredBrowserList(
                     config.mode,
                     list,
                     nonBrowsers,
-                    isSingleOption = isSingleOption
+                    isSingleOption = hasInfo
                 )
             }
 

app/src/test/java/fe/linksheet/module/resolver/ImprovedBrowserHandlerTest.kt
@@ -133,7 +133,7 @@ internal class ImprovedBrowserHandlerTest : BaseUnitTest  {
                 browserMode = BrowserMode.SelectedBrowser,
                 browsers = listOfFirstActivityResolveInfo(PackageInfoFakes.MiBrowser),
                 apps = allAppsInfoList,
-                isSingleOption = false,
+                isSingleOption = true,
                 noBrowsersOnlySingleApp = false
             )
         )

commit ac733fc04f220f186354f23f9c62ad62f5159044

#696 fix: Auto launch regression introduced in 567753d

app/src/main/java/fe/linksheet/module/resolver/ImprovedBrowserHandler.kt
@@ -53,17 +53,14 @@ class ImprovedBrowserHandler {
 
             is BrowserModeConfigHelper.SelectedBrowser -> {
                 val browserResolveInfo = config.selectedBrowser?.let { getBrowser(browsers, it) }
-                val hasInfo = browserResolveInfo != null
 //                // TODO: Need to use merged here since resolvedList might contain ResolveInfos also present in browsers
 //                // TODO: Do we really need to use the component?
-//                val isSingleOption = nonBrowsers.isEmpty()
-//                        && browsers.size == 1
-//                        && browsers.values.singleOrNull()?.activityInfo?.componentName == browserResolveInfo?.activityInfo?.componentName
+                val isSingleOption = nonBrowsers.isEmpty() && browserResolveInfo != null
                 FilteredBrowserList(
                     browserMode = config.mode,
-                    browsers = if (hasInfo) listOf(browserResolveInfo) else emptyList(),
+                    browsers = listOfNotNull(browserResolveInfo),
                     apps = nonBrowsers,
-                    isSingleOption = hasInfo
+                    isSingleOption = isSingleOption
                 )
             }
 

app/src/test/java/fe/linksheet/module/resolver/ImprovedBrowserHandlerTest.kt
@@ -121,7 +121,7 @@ internal class ImprovedBrowserHandlerTest : BaseUnitTest  {
     }
 
     @org.junit.Test
-    fun `selected browser`() {
+    fun `selected browser, multiple browsers, multiple native apps`() {
         val config = BrowserModeConfigHelper.SelectedBrowser(PackageInfoFakes.MiBrowser.packageInfo.packageName)
 
         val result = handler.filterBrowsers(config, false,allBrowsersResolveInfos, allResolvedInfoList)
@@ -130,6 +130,22 @@ internal class ImprovedBrowserHandlerTest : BaseUnitTest  {
                 browserMode = config.mode,
                 browsers = listOfFirstActivityResolveInfo(PackageInfoFakes.MiBrowser),
                 apps = allAppsInfoList,
+                isSingleOption = false,
+                noBrowsersOnlySingleApp = false
+            )
+        )
+    }
+
+    @org.junit.Test
+    fun `selected browser, multiple browsers, no native apps`() {
+        val config = BrowserModeConfigHelper.SelectedBrowser(PackageInfoFakes.MiBrowser.packageInfo.packageName)
+
+        val result = handler.filterBrowsers(config, false,allBrowsersResolveInfos, allBrowsersResolveInfos)
+        assertThat(result).isDataClassEqualTo(
+            FilteredBrowserList(
+                browserMode = config.mode,
+                browsers = listOfFirstActivityResolveInfo(PackageInfoFakes.MiBrowser),
+                apps = emptyList(),
                 isSingleOption = true,
                 noBrowsersOnlySingleApp = false
             )
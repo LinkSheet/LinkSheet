c5cdda2262a1da8286859d845588f41b6a3970e3
1fexd <PGFvy85nC@protonmail.com>
2026-05-01 21:05:29 +0200

    chore: Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     kotlin("plugin.compose") apply false
     kotlin("plugin.serialization") apply false
     id("com.android.application") apply false
-    id("net.nemerosa.versioning") apply false
     id("androidx.room") apply false
     id("com.google.devtools.ksp") apply false
     id("com.gitlab.grrfe.android-build-plugin") apply false

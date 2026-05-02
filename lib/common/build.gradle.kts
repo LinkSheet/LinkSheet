63029d2060419e225bd330a61c938e53e77e21b7
1fexd <PGFvy85nC@protonmail.com>
2026-05-02 14:39:51 +0200

    chore: Bump AGP

--- lib/common/build.gradle.kts
+++ lib/common/build.gradle.kts
@@ -1,4 +1,4 @@
-
+import com.android.build.api.dsl.AndroidLibrarySourceSet
 import com.gitlab.grrfe.gradlebuild.Version
 import com.gitlab.grrfe.gradlebuild.android.AndroidSdk
 import fe.build.dependencies._1fexd
@@ -20,10 +20,9 @@ android {
         jvmToolchain(Version.JVM)
     }
 
-    val main by sourceSets
-    for (it in arrayOf("compat")) {
-        main.kotlin.directories.add("src/main/$it")
-    }
+    // Need to cast to com.android.build.gradle.dsl.AndroidLibrarySourceSet, otherwise the following exception is thrown:
+//    class com.android.build.gradle.internal.api.DefaultAndroidLibrarySourceSet_Decorated cannot be cast to class com.android.build.gradle.api.AndroidLibrarySourceSet (com.android.build.gradle.internal.api.DefaultAndroidLibrarySourceSet_Decorated and com.android.build.gradle.api.AndroidLibrarySourceSet are in unnamed module of loader org.gradle.internal.classloader.VisitableURLClassLoader$InstrumentingVisitableURLClassLoader @2a7ab924)
+    (sourceSets["main"] as AndroidLibrarySourceSet).kotlin.directories.add("src/main/compat")
 }
 
 dependencies {

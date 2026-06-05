517368c5b2fffff8db7a3a771381c24a81a14e2c
1fexd <PGFvy85nC@protonmail.com>
2026-06-05 10:27:19 +0200

    fix: Duplicate class

--- lib/util/src/main/kotlin/fe/linksheet/extension/android/ActivityExt.kt
+++ lib/util/src/main/kotlin/fe/linksheet/extension/android/ActivityExt.kt
@@ -1,10 +1,39 @@
 package fe.linksheet.extension.android
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.widget.Toast
+import androidx.annotation.StringRes
 import fe.std.result.IResult
 import fe.std.result.tryCatch
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 fun Activity.tryStartActivity(intent: Intent): IResult<Unit> {
     return tryCatch { startActivity(intent) }
+}
+
+suspend fun Context.showToast(
+    @StringRes textId: Int,
+    duration: Int = Toast.LENGTH_SHORT
+) = withContext(Dispatchers.Main) {
+    Toast.makeText(this@showToast, getString(textId), duration).show()
+}
+
+fun Activity.showToast(
+    @StringRes textId: Int,
+    duration: Int = Toast.LENGTH_SHORT,
+    uiThread: Boolean = false,
+) = showToast(getString(textId), duration, uiThread)
+
+fun Activity.showToast(
+    text: String,
+    duration: Int = Toast.LENGTH_SHORT,
+    uiThread: Boolean = false,
+) {
+    val toast = { Toast.makeText(this, text, duration).show() }
+
+    if (uiThread) runOnUiThread(toast)
+    else toast()
 }

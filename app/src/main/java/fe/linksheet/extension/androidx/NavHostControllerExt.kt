1408404fa67f841401c20c35cf5c9cef6650aca1
1fexd <PGFvy85nC@protonmail.com>
2026-06-05 13:19:38 +0200

    chore: Remove unused ext

--- app/src/main/java/fe/linksheet/extension/androidx/NavHostControllerExt.kt
+++
@@ -1,7 +0,0 @@
-package fe.linksheet.extension.androidx
-
-import androidx.navigation.NavHostController
-
-fun NavHostController.navigate(vararg routes: String) {
-    for (route in routes) navigate(route)
-}
